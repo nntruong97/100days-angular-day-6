36490654d8b04fbf86775b4ac5e852d034b4fb2e
nntruong97 <nguyennhattruongspkt@gmail.com>
2021-12-16 21:28:24 +0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  isDanger= false;
+  isWarning= false;
+
+  classes = 'box red-border backgroud-color';
 }
